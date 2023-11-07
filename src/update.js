b0e5e536af76942066d53492fa91df252f4f6400
robo-mop <realaryanpingle@gmail.com>
2023-11-07 21:38:45 +0530

    First and last

--- src/update.js
+++ src/update.js
@@ -1,8 +1,6 @@
-import format from 'date-fns/format';
-
 var span = document.querySelector('#time-now');
 
 export default function update() {
-	span.textContent = format(new Date(), 'h:mm:ssa');
+	span.textContent = new Date().toLocaleTimeString();
 	setTimeout(update, 1000);
 }
